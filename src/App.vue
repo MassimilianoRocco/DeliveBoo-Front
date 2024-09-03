0205df41fa7fdabf8543dd731d847e9d80dc66ba
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-09-03 16:31:26 +0200

    scroll up button, restaurants/category/card components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,81 @@
 <script>
+import AppRestaurants from "./views/AppRestaurants.vue";
 
+export default {
+	name: "App",
+	components: {
+		AppRestaurants,
+	},
 
-
-
-export default{
-  
-
-  components:{
-   
- 
-  },
-
-  data(){
-    return{
-
-    }
-  },
-
-
-mounted(){
-
-}
-
-}
+	data() {
+		return {
+			checkScroll: false,
+		};
+	},
+	methods: {
+		backToTop() {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		},
+		handleScroll() {
+			if (window.scrollY > 20) {
+				this.checkScroll = true;
+			} else {
+				this.checkScroll = false;
+			}
+		},
+	},
+	mounted() {
+		window.addEventListener(`scroll`, this.handleScroll);
+	},
+};
 </script>
 
 <template>
+	<div class="app_Container w-100">
+		<AppRestaurants />
 
-  <div class="app_Container w-100">
-
-
-  </div>
-
+		<button
+			type="button"
+			class="btn position-fixed rounded-circle border border-3 border-black"
+			id="btn-scroll-top"
+			v-if="checkScroll"
+			@click="backToTop">
+			<div class="background"></div>
+			<i class="fas fa-arrow-up"></i>
+		</button>
+	</div>
 </template>
 
 <style scoped>
+#btn-scroll-top {
+	bottom: 20px;
+	right: 20px;
+	background-color: white;
+	z-index: 1;
+	overflow: hidden;
+}
 
+.background {
+	width: 0;
+	height: 0;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-image: linear-gradient(#000000, #000000);
+	transition: 0.5s;
+	z-index: -1;
+}
+
+button:hover .background {
+	width: 150%;
+	height: 150%;
+}
+
+button:hover i {
+	color: white;
+}
 </style>
---
+++ src/views/AppRestaurants.vue
@@ -0,0 +1,73 @@
+<script>
+import axios from "axios";
+import Categories from "./AppRestaurantsComponents/Categories.vue";
+import RestaurantsCard from "./AppRestaurantsComponents/RestaurantsCard.vue";
+
+export default {
+	name: "AppRestaurants",
+	components: {
+		Categories,
+		RestaurantsCard,
+	},
+	data() {
+		return {
+			// posts: "",
+		};
+	},
+	methods: {
+		// changePage(url) {
+		// 	axios.get(url).then((result) => {
+		// 		this.posts = result.data.posts;
+		// 	});
+		// },
+	},
+	mounted() {
+		// axios.get("http://localhost:8000/api/posts").then((result) => {
+		// 	console.log(result.data.posts);
+		// 	this.posts = result.data.posts;
+		// 	console.log(this.posts.length);
+		// });
+	},
+};
+</script>
+
+<template>
+	<div class="container">
+		<div class="col text-center">
+			<h1 class="text-center fw-bold display-5 my-5 text-warning">SCEGLI IL TUO RISTORANTE</h1>
+		</div>
+
+		<!-- <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 mb-5"> -->
+		<Categories />
+		<!-- </div> -->
+
+		<div class="text-center">
+			<h1 class="text-center fw-bold display-5 my-5 text-warning">LISTA RISTORANTI</h1>
+		</div>
+
+		<div class="row mx-0">
+			<div v-for="i in 12" :key="i" class="col-3 p-3">
+				<RestaurantsCard />
+			</div>
+		</div>
+
+		<!-- <RestaurantsCard v-for="post in posts.data" :element="post" />
+
+			<nav aria-label="Page navigation">
+				<ul class="pagination">
+					<li
+						v-for="link in posts.links"
+						class="page-item"
+						:class="{ disabled: !link.url, active: link.active }">
+						<a
+							href="#"
+							class="page-link"
+							@click="changePage(link.url)"
+							v-html="link.label"></a>
+					</li>
+				</ul>
+			</nav> -->
+	</div>
+</template>
+
+<style></style>
