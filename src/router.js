4394cb56d31d0ce2f598e1d706d1155f98464a3f
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-09-03 11:28:01 +0200

    Created vite project folder and added router

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import AppHome from "./views/AppHome.vue";
+// import AppAbout from "./views/AppAbout.vue";
+// import AppProjects from "./views/AppProjects.vue";
+// import AppContacts from "./views/AppContacts.vue";
+// import SingleProject from "./views/SingleProject.vue";
+// import NotFound from "./views/NotFound.vue";
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: AppHome,
+		},
+	],
+});
+
+export { router };
