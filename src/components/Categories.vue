54d93eee158d5ca3d9796e1965c3bf10627eaca7
davidearbola <davidearbola@gmail.com>
2024-09-07 10:54:21 +0200

    fixed greyscale category

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,31 +1,32 @@
 <script>
 import axios from "axios";
-import store from '../store/store'
+import store from "../store/store";
 export default {
 	name: "Categories",
 	data() {
 		return {
 			categories: [],
-			clickedCategories: [],
+			selectedCategories: [],
 			store,
 			category: "",
 		};
 	},
 	methods: {
 		selectCategory(indice) {
-			console.log(indice);
-
 			const categoryId = indice + 1;
-			if (this.store.categoriesSelected.includes(categoryId)) {
-				this.store.categoriesSelected = this.store.categoriesSelected.filter(
-					(id) => id !== categoryId
-				);
+
+			if (this.selectedCategories.includes(indice)) {
+				this.selectedCategories = this.selectedCategories.filter((i) => i !== indice);
+				this.store.categoriesSelected = this.store.categoriesSelected.filter((id) => id !== categoryId);
 			} else {
+				this.selectedCategories.push(indice);
 				this.store.categoriesSelected.push(categoryId);
 			}
+
 			this.store.requestRestaurants.params = {
 				categories: this.store.categoriesSelected,
 			};
+
 			axios
 				.get(this.store.requestRestaurants.url, {
 					params: this.store.requestRestaurants.params,
@@ -35,11 +36,9 @@ export default {
 					store.restaurants = response.data.restaurants;
 					console.log(store.restaurants);
 				});
-			if (this.clickedCategories.includes(indice)) {
-				this.clickedCategories = this.clickedCategories.filter((i) => i !== indice);
-			} else {
-				this.clickedCategories.push(indice);
-			}
+		},
+		isCategorySelected(indice) {
+			return this.selectedCategories.includes(indice);
 		},
 	},
 	mounted() {
@@ -54,9 +53,12 @@ export default {
 <template>
 	<div class="row mx-0 px-0 justify-content-center">
 		<div v-for="(category, indice) in categories" :key="category" class="col-6 col-sm-4 col-md-3 col-lg-2 p-3">
-			<div id="img_container" class="w-100 shadow-lg rounded-4 p-2 border border-2 position-relative"
-				@click="selectCategory(indice)" :class="{ active: clickedCategories.includes(indice) }">
-				<img class="img-fluid mx-auto d-block " :src="category.image_path" alt="" style="height: 100px;" />
+			<div
+				id="img_container"
+				class="w-100 shadow-lg rounded-4 p-2 border border-2 position-relative"
+				@click="selectCategory(indice)"
+				:class="{ grayscale: !isCategorySelected(indice) && selectedCategories.length > 0 }">
+				<img class="img-fluid mx-auto d-block" :src="category.image_path" alt="" style="height: 100px" />
 				<div
 					class="position-absolute top-100 start-50 translate-middle text-dark bg-white text-center fs-5 fw-bold text-nowrap badge rounded-pill">
 					{{ category.name }}
@@ -73,10 +75,7 @@ export default {
 	cursor: pointer;
 }
 
-.active {
+.grayscale {
 	filter: grayscale(100%);
-	/* border: 2px solid #ccc;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-	scale: 1.15; */
 }
 </style>
